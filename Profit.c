f9d561fb0a9a540a4c0700090f30300c5c14b93d
kowshik jallipalli <119750794+kowshikjallipalli0105@users.noreply.github.com>
2022-12-26 21:59:42 +0530

    #include<stdio.h> int main() {     int cp,sp;     scanf("%d%d",&sp,&cp);     if(sp<cp)     {         printf("Profit");     }     else     {         printf("Loss");     } }

---
+++ Profit.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int cp,sp;
+    scanf("%d%d",&sp,&cp);
+    if(sp<cp)
+    {
+        printf("Profit");
+    }
+    else
+    {
+        printf("Loss");
+    }
+}
