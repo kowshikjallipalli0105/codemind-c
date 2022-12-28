9a979f81087c2f8178173d7dedc9b03fc974d465
kowshik jallipalli <119750794+kowshikjallipalli0105@users.noreply.github.com>
2022-12-28 21:56:40 +0530

    #include<stdio.h> int main(){     int n,n1;     scanf("%d",&n);     for(int i=1;i<=n;i++)     {         scanf("%d",&n1);         if(n1>30)         {             printf("YES ");         }         else         {             printf("NO ");         }     } }

---
+++ Subscription.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int n,n1;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        scanf("%d",&n1);
+        if(n1>30)
+        {
+            printf("YES
+");
+        }
+        else
+        {
+            printf("NO
+");
+        }
+    }
+}
