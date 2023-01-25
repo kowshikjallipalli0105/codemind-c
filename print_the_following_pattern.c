a09dbb846bcaecc6938ea3792229e9445419c6b2
kowshik jallipalli <119750794+kowshikjallipalli0105@users.noreply.github.com>
2023-01-25 13:52:57 +0530

    #include<stdio.h> int main(){     int n;     char i,j;     scanf("%d",&n);     for(i=64+n;i>=65;i--)     {         for(j=i;j>=65;j--)         printf("%c ",i);         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
-int main()
-{
-    int n,i,j;
+int main(){
+    int n;
+    char i,j;
     scanf("%d",&n);
-    for(i=n;i>=1;i--)
+    for(i=64+n;i>=65;i--)
     {
-        for(j=n;j>=1;j--)
-        printf("%d ",j);
+        for(j=i;j>=65;j--)
+        printf("%c ",i);
         printf("
 ");
     }
